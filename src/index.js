9d26f179a3455a378081cc2fd02b63a020644326
Bogdan Barbu <bogdan.b@codingheads.com>
2021-08-13 14:13:36 +0300

    export FixHashScrollPosition

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export { ScrollDirection } from './ScrollDirection';
 export { ScrollOffset, ScrollOffsetPart } from './ScrollOffset';
 export { ScrollPages } from './ScrollPages';
 export { scrollWithMarginTop } from './scrollWithMarginTop';
+export { FixHashScrollPosition } from './FixHashScrollPosition';
 export { BootstrapAccordionScrollIntoView } from './BootstrapAccordionScrollIntoView';
 export { FoundationAccordionScrollIntoView } from './FoundationAccordionScrollIntoView';
